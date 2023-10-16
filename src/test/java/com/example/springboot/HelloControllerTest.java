855c17c0386b31138a2a38b18a7422fd323f3365
Mae Large <mlarge@vmware.com>
2023-10-16 14:51:55 -0700

    change 5

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings - change 4, yeet!", controller.index());
+        assertEquals("Greetings - change 5, yeet!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings - change 4, yeet!"));
+            .andExpect(content().string("Greetings - change 5, yeet!"));
     }
 }
