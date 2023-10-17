193a953be3cbf09927c132a0d7f0504e31ab671e
Mae Large <mlarge@vmware.com>
2023-10-17 07:53:33 -0700

    16

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings - change 15 yeet!";
+		return "Greetings - change 16 yeet!";
 	}
 
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings - change 15 yeet!", controller.index());
+        assertEquals("Greetings - change 16 yeet!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings - change 15 yeet!"));
+            .andExpect(content().string("Greetings - change 16 yeet!"));
     }
 }
